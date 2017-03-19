6054c68232fcca33cf5d34ccb8d4c2c338b2549e
Charles Severance <csev@umich.edu>
2017-03-19 00:14:56 -0400

    Add links

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
+<html>
+<head>
+<style>
+a[target="_blank"]:after {
+  content: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg==);
+  margin: 0 3px 0 5px;
+}
+</style>
+</head>
+<body>
 <h1>Testing Some Silex Stuff</h1>
 <ul>
 <li><a href="session/hello/bob" target="_blank">Test Legacy Sessions</a> using
 <a href="http://symfony.com/doc/current/components/http_foundation/session_php_bridge.html"
-target="_blank">Symfony PhpBridgeSessionStorage</a>.  Just keep pressing refresh.
+target="_blank">Symfony PhpBridgeSessionStorage</a>. 
 </li>
 <li>Testing different template loaders
 <ul>
@@ -12,3 +22,4 @@ target="_blank">Symfony PhpBridgeSessionStorage</a>.  Just keep pressing refresh
 </ul>
 <li>Show <a href="level1/level2/hello/bob" target="_blank">Show that silex routes are relative</a></li>
 </ul>
+</body>
